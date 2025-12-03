7403681fd0ff330b41d5ac8b734aeb6644039c35
Kaush <superkaush@gmail.com>
2025-12-02 21:40:12 -0800

    some fixes

--- shell.c
+++ shell.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 int main(int argc, char **argv){
 	
 	shell_loop();
@@ -23,18 +24,17 @@ void shell_loop(void){
 	}while(status);
 }
 
-#define SHELL_BUFSIZE 1024
+#define LINE_BUFSIZE 1024
 char *read_line(void){
-	int bufsize = SHELL_BUFSIZE;
-	char *buffer = malloc(size_of(char) * bufsize);
+	int bufsize = LINE_BUFSIZE;
+	char *buffer = malloc(sizeof(char) * bufsize);
 	int position = 0;
 	int c;
 	
 	if (!buffer){
-		fprintf(stderr,"shell: error allocating memory to buffer");
+		fprintf(stderr,"shell: error allocating memory to buffer\n");
 		exit (EXIT_FAILURE);
 	}
-	
 	while(1){
 		c  = getchar();
 		if (c == EOF || c == '\n'){
@@ -46,13 +46,46 @@ char *read_line(void){
 		position++;
 
 		if(position >= bufsize){
-			bufsize += SHELL_BUFSIZE;
+			bufsize += LINE_BUFSIZE;
 			buffer = realloc(buffer,bufsize);
 		
 			if (!buffer){
-				fprintf(stderr,"shell: error allocating memory to buffer");
+				fprintf(stderr,"shell: error allocating memory to buffer\n");
 				exit (EXIT_FAILURE);
 			}
 		}
 	}
 }
+
+#define TOK_SIZE 64
+#define DELIM "\t\r\a\n"
+def char **split_line(char *line){
+	int bufsize = TOK_SIZE;
+	char **tokens = malloc(sizeof(char*) * bufsize);
+	char *token;
+	int pos = 0;
+
+	if (!tokens){
+		fprintf(stderr,"shell: error allocating memory for tokens");
+		exit(EXIT_FAILURE);
+	}
+	
+	token = strtok(line, DELIM);
+	while(token != NULL){
+		tokens[pos] = token;
+		pos++;
+		
+		if(pos >= bufsize){
+			bufsize += TOK_SIZE;
+			tokens = realloc(tokens, sizeof(char*) * bufsize);
+			if (!tokens){
+				fprintf(stderr, "shell: error reallocating memory for tokens");
+				exit(EXIT_FAILURE);
+			}
+		}
+		token = strtok(NULL,DELIM);
+	} 
+	tokens[pos] = NULL;
+	return tokens;
+}
+
